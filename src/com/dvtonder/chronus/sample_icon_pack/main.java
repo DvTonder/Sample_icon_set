7bd3a570d2f4c743a27d03db419f6069a43c6091
DvTonder <david.vantonder@gmail.com>
2014-01-08 19:04:57 -0500

    Initial commit

---
+++ src/com/dvtonder/chronus/sample_icon_pack/main.java
@@ -0,0 +1,14 @@
+package com.dvtonder.chronus.sample_icon_pack;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class main extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
