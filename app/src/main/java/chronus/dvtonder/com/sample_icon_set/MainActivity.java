91bb5ccb19e78ab6e3edeb3da099b8c372d85b05
Filippo Favaro <faavaro@gmail.com>
2015-08-03 17:29:44 +0200

    Switch project to Android Studio

---
+++ app/src/main/java/chronus/dvtonder/com/sample_icon_set/MainActivity.java
@@ -0,0 +1,14 @@
+package chronus.dvtonder.com.sample_icon_set;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+
+}
